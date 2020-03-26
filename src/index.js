e610d3127da991cc0646c5be8907573661506c7f
GulajavaMinistudio <gulajava.mini@gmail.com>
2020-03-26 10:32:22 +0700

    Pembuatan debugger contoh untuk CLI Node

--- src/index.js
+++ src/index.js
@@ -25,3 +25,23 @@ console.log(counterObject.getCounter());
 const newCounterObject = require('./appmodul');
 
 console.log('counter instance baru', newCounterObject.getCounter());
+
+/**
+ * Latihan menggunakan Debugger di Node JS
+ * Jalankan node inspect app.js , maka akan masuk ke debugger
+ * ketik c untuk continue proses sampai akhir
+ * ketik n untuk jalankan node sampai baris berikut
+ * ketik s untuk masuk ke dalam fungsi dari baris yang di debug
+ * ketik o untuk keluar dari eksekusi fungsi setelah menekan s
+ *
+ * Ketika masuk mode debugger, bisa mengetikkan repl untuk ke mode
+ * interaktif dan mulai inspect parameter yang ada
+ */
+
+/**
+ * Jalankan node --inspect index.js
+ * Menjalankan debugger interaktif dengan bantuan Chrome browser
+ * Buka Chrome dan akses chrome://inspect
+ * Atau bisa juga dengan jalankan node --inspect-brk index.js
+ * Untuk menjalankan debugger di Chrome dan memasang break point di dalamnya
+ */
